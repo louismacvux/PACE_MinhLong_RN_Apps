3c4b0f7c4ba6508dd4db455f7dee18b31a5d7f3e
louismacvux <57550224+louismacvux@users.noreply.github.com>
2024-12-06 14:37:26 -0600

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={{ padding: 10 }}>
+        <Text style={styles.header}>Minh Long Vu</Text>
+        <View>
+          <Text style={styles.head1}>Personal info</Text>
+          <Text>Email: minhlong9696@gmail.com</Text>
+          <Text>Phone number: (204) 123 4567</Text>
+        </View>
+        <View>
+          <Text style={styles.head1}>Interest</Text>
+          <Text>Listen to Music</Text>
+          <Text>Watch Movie</Text>
+        </View>
+        <View>
+          <Text style={styles.head1}>Professional Skills</Text>
+          <Text>Software Developer</Text>
+          <Text>Project Manager</Text>
+        </View>
+        <View>
+          <Text style={styles.head1}>Work Experience</Text>
+          <Text style={{ fontWeight: "bold" }}>Languageline Solutions</Text>
+          <Text>Interpreter/Translator</Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  head1: {
+    fontSize: 22,
+    fontWeight: "semibold",
+    marginTop: 10,
+  },
+  job: {
+    textAlign: "right",
   },
 });
